9652bfcc4d7570746cb8e5fa8a2a187df35c096b
Papoulas, Konstantin <konstantin.papoulas@kumavision.com>
2021-10-23 13:50:53 +0200

    feat YattViewer: enable local scripts and css

--- TranslationExtension/src/extension.ts
+++ TranslationExtension/src/extension.ts
@@ -1,43 +1,20 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 import { DepNodeProvider, Dependency } from './nodeDependencies';
 import { YattViewer } from './YattViewer';
 
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('yatt: open translation viewer', () => {			
+			YattViewer.createOrShow(context.extensionUri);
+		})
+	);
 
-	// Samples of `window.registerTreeDataProvider`
-	//const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
-	// vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
-	// vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	// vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	// vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	// vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	// vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
-	
-	// Test View
-	//new TranslatableElementsView(context);
-	const yattViewer = new YattViewer(context);
-
-	vscode.commands.registerCommand('yatt: open translation viewer',()=> {
-		const panel = vscode.window.createWebviewPanel(
-				'yattTranslationViewer',  // Identifies the type of the webview. Used internally
-				'YATT Translation Viewer', // Title of the panel displayed to the user
-				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-				{
-					enableScripts: true
-				} // Webview options
-		);
-
+	vscode.commands.registerCommand('yatt: refresh translation viewer', () => {
 		// And set its HTML content		
-		yattViewer.getWebviewContent().then(webViewerContent => {
-			panel.webview.html = webViewerContent;			
-		});				
-	});		
-
-	vscode.commands.registerCommand('yatt: refresh translation viewer',()=> {		
-		// And set its HTML content		
-		yattViewer.updateWebview();
-	});		
+		//yattViewer.updateWebview();
+	});
 }
--- TranslationExtension/src/yattViewer.ts
+++ TranslationExtension/src/yattViewer.ts
@@ -1,53 +1,206 @@
 import { TextDecoder } from 'util';
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+	return {
+		// Enable javascript in the webview
+		enableScripts: true,
+
+		// And restrict the webview to only loading content from our extension's `media` directory.
+		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'assets'), vscode.Uri.joinPath(extensionUri, 'resources')]
+	};
+}
 
 export class YattViewer {
-	constructor(context: vscode.ExtensionContext) {
-		// const view = vscode.window.createTreeView('testView', { treeDataProvider: aNodeWithIdTreeDataProvider(), showCollapseAll: true });
-		// context.subscriptions.push(view);
-		// vscode.commands.registerCommand('testView.reveal', async () => {
-		// 	const key = await vscode.window.showInputBox({ placeHolder: 'Type the label of the item to reveal' });
-		// 	if (key) {
-		// 		await view.reveal({ key }, { focus: true, select: false, expand: true });
-		// 	}
-		// });
-		// vscode.commands.registerCommand('testView.changeTitle', async () => {
-		// 	const title = await vscode.window.showInputBox({ prompt: 'Type the new title for the Test View', placeHolder: view.title });
-		// 	if (title) {
-		// 		view.title = title;
-		// 	}
-		// });
+	public static currentPanel: YattViewer | undefined;
+	public static readonly viewType = 'yattViewer';
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionUri: vscode.Uri;
+	private _disposables: vscode.Disposable[] = [];
 
-		// TODO: Demodaten anzeigen.
-		// TODO: Demodaten zur Laufzeit erzeugen.
+	public static createOrShow(extensionUri: vscode.Uri) {
+		const column = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+		// If we already have a panel, show it.
+		if (YattViewer.currentPanel) {
+			YattViewer.currentPanel._panel.reveal(column);
+			return;
+		}
+
+		// Otherwise, create a new panel.
+		const panel = vscode.window.createWebviewPanel(
+			YattViewer.viewType,
+			'YaTT Viewer',
+			column || vscode.ViewColumn.One,
+			getWebviewOptions(extensionUri),
+		);
+
+		YattViewer.currentPanel = new YattViewer(panel, extensionUri);
 	}
 
-	public getWebviewContent(): Thenable<string> {
-		const p = new Promise<string>((resolve, reject) => {	
-			const extension = vscode.extensions.getExtension('KonnosPB.al-yatt');
-			const extensionUri = extension.extensionUri;								
-			const yattViewerUri = vscode.Uri.joinPath(extensionUri, '/resources/yattViewer.html');
-			const jQueryJsUri = vscode.Uri.joinPath(extensionUri, '/assets/jquery/jquery-3.6.0.js');
-			const jQueryUiJsUri = vscode.Uri.joinPath(extensionUri, '/assets/jquery/jquery-ui.js');
-			const tabulatorJsUri = vscode.Uri.joinPath(extensionUri, '/assets/tabulator/js/tabulator.js');
-			const tabulatorCssUri = vscode.Uri.joinPath(extensionUri, '/assets/tabulator/css/tabulator.css');
-			const progressTrackerCssUri = vscode.Uri.joinPath(extensionUri, '/assets/progress-tracker/progress-tracker.css');
-			vscode.workspace.fs.readFile(yattViewerUri)
-				.then(unit8Arr => {
-					const webViewerContent = new TextDecoder("utf-8").decode(unit8Arr)				
-					.replace("%JQUERY_JS%", jQueryJsUri.fsPath)
-					.replace("%JQUERY_UI_JS%", jQueryUiJsUri.fsPath)
-					.replace("%TABULATOR_JS%", tabulatorJsUri.fsPath)
-					.replace("%YATT_JS%", "")					
-					.replace("%TABULATOR_CSS%", tabulatorCssUri.fsPath)
-					.replace("%PROGRESS_TRACKER_CSS%", progressTrackerCssUri.fsPath);					
-					resolve(webViewerContent);
+	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		YattViewer.currentPanel = new YattViewer(panel, extensionUri);
+	}
+
+	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		this._panel = panel;
+		this._extensionUri = extensionUri;
+
+		// Set the webview's initial html content
+		this._update();
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programmatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Update the content based on view changes
+		this._panel.onDidChangeViewState(
+			e => {
+				if (this._panel.visible) {
+					this._update();
+				}
+			},
+			null,
+			this._disposables
+		);
+
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			null,
+			this._disposables
+		);
+	}
+
+	public doRefactor() {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+
+	public dispose() {
+		YattViewer.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _update() {
+		const webview = this._panel.webview;
+
+		// Vary the webview's content based on where it is located in the editor.
+		this._panel.title = 'YaTT Viewer';
+		this._panel.webview.html = this._getHtmlForWebview(webview);
+	}
+
+
+	private _getHtmlForWebview(webview: vscode.Webview) {					
+		const extensionPath = this._extensionUri;
+		const jQueryPath = vscode.Uri.joinPath(extensionPath, 'assets', 'jquery', 'jquery-3.6.0.js');
+		const jQueryUIPath = vscode.Uri.joinPath(extensionPath, 'assets', 'jquery-ui', 'jquery-ui.js');
+		const tabulatorPath = vscode.Uri.joinPath(extensionPath, 'assets', 'tabulator', 'js', 'tabulator.js');
+		const tabulatorCssPath = vscode.Uri.joinPath(extensionPath, 'assets', 'tabulator', 'css', 'tabulator.css');
+		const progressTrackerPath = vscode.Uri.joinPath(extensionPath, 'assets', 'progress-tracker', 'progress-tracker.css');
+
+		const jQueryUri = webview.asWebviewUri(jQueryPath);		
+		const jQueryUIUri = webview.asWebviewUri(jQueryUIPath);		
+		const tabulatorUri = webview.asWebviewUri(tabulatorPath);		
+		const tabulatorCssUri = webview.asWebviewUri(tabulatorCssPath);		
+		const progressTrackerUri = webview.asWebviewUri(progressTrackerPath);
+		const yatt_js = "";
+		
+		const yattViewerHtml = `<!DOCTYPE html>
+		<html>		
+		<head>
+			<meta charset="utf-8">
+			<script nonce="${getNonce()}" src="${jQueryUri}" ></script>
+			<script nonce="${getNonce()}" src="${jQueryUIUri}"></script>
+			<script nonce="${getNonce()}" src="${tabulatorUri}" ></script>
+			<script nonce="${getNonce()}" src="${yatt_js}" ></script>
+			<link href="${tabulatorCssUri}" rel="stylesheet">
+			<link href="${progressTrackerUri}" rel="stylesheet">		
+		</head>		
+		<body>		
+			<script>
+				var table = new Tabulator("#example-table", {
+					columns: [
+						{ title: "Name", field: "name" },
+						{ title: "Progress", field: "progress", sorter: "number" },
+						{ title: "Gender", field: "gender" },
+						{ title: "Rating", field: "rating" },
+						{ title: "Favourite Color", field: "col" },
+						{ title: "Date Of Birth", field: "dob", hozAlign: "center" },
+					],
 				});
-		});
-		return p;
-	}
+			</script>					
+			<div id="example-table"></div>		
+			<div class="progress-container" style="visibility:hidden;">
+				<ul class="progress-tracker progress-tracker--text progress-tracker--center">
+					<li class="progress-step is-complete">
+						<div class="progress-marker"></div>
+						<div class="progress-text">
+							<h4 class="progress-title">Step 1</h4>
+							Summary text explaining this step to the user
+						</div>
+					</li>		
+					<li class="progress-step is-complete">
+						<div class="progress-marker"></div>
+						<div class="progress-text">
+							<h4 class="progress-title">Step 2</h4>
+							Summary text explaining this step to the user
+						</div>
+					</li>		
+					<li class="progress-step is-active" aria-current="step">
+						<div class="progress-marker"></div>
+						<div class="progress-text">
+							<h4 class="progress-title">Step 3</h4>
+							Summary text explaining this step to the user
+						</div>
+					</li>		
+					<li class="progress-step">
+						<div class="progress-marker"></div>
+						<div class="progress-text">
+							<h4 class="progress-title">Step 4</h4>
+							Shorter summary text
+						</div>
+					</li>		
+					<li class="progress-step">
+						<div class="progress-marker"></div>
+						<div class="progress-text">
+							<h4 class="progress-title">Step 5</h4>
+							Shorter summary text
+						</div>
+					</li>
+				</ul>
+			</div>
+		</body>		
+		</html>`;
+		return yattViewerHtml;
+	}		
+}
 
-	public updateWebview(): void {
-		// TODO implement updateWebview;
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	}
+	return text;
 }

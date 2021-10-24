7a5210a5e6a9624f10426ded53d1a47895686c9c
Papoulas, Konstantin <konstantin.papoulas@kumavision.com>
2021-10-24 08:56:42 +0200

    feat: yattViewer postmessages

--- TranslationExtension/src/yattViewer.ts
+++ TranslationExtension/src/yattViewer.ts
@@ -2,6 +2,9 @@ import { TextDecoder } from 'util';
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
+//import * as ls from 'LanguageService';
+import { LanguageService } from './languageService';
+import { kill } from 'process';
 
 function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
 	return {
@@ -108,10 +111,23 @@ export class YattViewer {
 		// Vary the webview's content based on where it is located in the editor.
 		this._panel.title = 'YaTT Viewer';
 		this._panel.webview.html = this._getHtmlForWebview(webview);
+		this._updateData();
+	}
+
+	private _updateData(){
+		// const languageService = LanguageService.getInstance(this._extensionUri);
+		// languageService.getLanguages()
+		// 	.then(languages => {
+		// 		languages.filter(l => { l.})
+		// 	});
+		// languageService.getDataGrid()
+		// .then(languages => {
+		// 	languages = null;
+		// });
 	}
 
 
-	private _getHtmlForWebview(webview: vscode.Webview) {					
+	private _getHtmlForWebview(webview: vscode.Webview) {
 		const extensionPath = this._extensionUri;
 		const jQueryPath = vscode.Uri.joinPath(extensionPath, 'assets', 'jquery', 'jquery-3.6.0.js');
 		const jQueryUIPath = vscode.Uri.joinPath(extensionPath, 'assets', 'jquery-ui', 'jquery-ui.js');
@@ -119,81 +135,28 @@ export class YattViewer {
 		const tabulatorCssPath = vscode.Uri.joinPath(extensionPath, 'assets', 'tabulator', 'css', 'tabulator.css');
 		const progressTrackerPath = vscode.Uri.joinPath(extensionPath, 'assets', 'progress-tracker', 'progress-tracker.css');
 
-		const jQueryUri = webview.asWebviewUri(jQueryPath);		
-		const jQueryUIUri = webview.asWebviewUri(jQueryUIPath);		
-		const tabulatorUri = webview.asWebviewUri(tabulatorPath);		
-		const tabulatorCssUri = webview.asWebviewUri(tabulatorCssPath);		
+		const jQueryUri = webview.asWebviewUri(jQueryPath);
+		const jQueryUIUri = webview.asWebviewUri(jQueryUIPath);
+		const tabulatorUri = webview.asWebviewUri(tabulatorPath);
+		const tabulatorCssUri = webview.asWebviewUri(tabulatorCssPath);
 		const progressTrackerUri = webview.asWebviewUri(progressTrackerPath);
 		const yatt_js = "";
-		
-		const yattViewerHtml = `<!DOCTYPE html>
-		<html>		
-		<head>
-			<meta charset="utf-8">
-			<script nonce="${getNonce()}" src="${jQueryUri}" ></script>
-			<script nonce="${getNonce()}" src="${jQueryUIUri}"></script>
-			<script nonce="${getNonce()}" src="${tabulatorUri}" ></script>
-			<script nonce="${getNonce()}" src="${yatt_js}" ></script>
-			<link href="${tabulatorCssUri}" rel="stylesheet">
-			<link href="${progressTrackerUri}" rel="stylesheet">		
-		</head>		
-		<body>		
-			<script>
-				var table = new Tabulator("#example-table", {
-					columns: [
-						{ title: "Name", field: "name" },
-						{ title: "Progress", field: "progress", sorter: "number" },
-						{ title: "Gender", field: "gender" },
-						{ title: "Rating", field: "rating" },
-						{ title: "Favourite Color", field: "col" },
-						{ title: "Date Of Birth", field: "dob", hozAlign: "center" },
-					],
-				});
-			</script>					
-			<div id="example-table"></div>		
-			<div class="progress-container" style="visibility:hidden;">
-				<ul class="progress-tracker progress-tracker--text progress-tracker--center">
-					<li class="progress-step is-complete">
-						<div class="progress-marker"></div>
-						<div class="progress-text">
-							<h4 class="progress-title">Step 1</h4>
-							Summary text explaining this step to the user
-						</div>
-					</li>		
-					<li class="progress-step is-complete">
-						<div class="progress-marker"></div>
-						<div class="progress-text">
-							<h4 class="progress-title">Step 2</h4>
-							Summary text explaining this step to the user
-						</div>
-					</li>		
-					<li class="progress-step is-active" aria-current="step">
-						<div class="progress-marker"></div>
-						<div class="progress-text">
-							<h4 class="progress-title">Step 3</h4>
-							Summary text explaining this step to the user
-						</div>
-					</li>		
-					<li class="progress-step">
-						<div class="progress-marker"></div>
-						<div class="progress-text">
-							<h4 class="progress-title">Step 4</h4>
-							Shorter summary text
-						</div>
-					</li>		
-					<li class="progress-step">
-						<div class="progress-marker"></div>
-						<div class="progress-text">
-							<h4 class="progress-title">Step 5</h4>
-							Shorter summary text
-						</div>
-					</li>
-				</ul>
-			</div>
-		</body>		
-		</html>`;
+
+		const yattViewerHtmlPath = vscode.Uri.joinPath(extensionPath, 'resources', 'yattViewer.html');
+		const yattViewerHtml = fs.readFileSync(yattViewerHtmlPath.fsPath)
+			.toString("UTF-8")
+			.replace("{{nonce_1}}", getNonce())
+			.replace("{{nonce_2}}", getNonce())
+			.replace("{{nonce_3}}", getNonce())
+			.replace("{{nonce_4}}", getNonce())
+			.replace("{{jquery_js}}", jQueryUri.toString())
+			.replace("{{jquery_ui_js}}", jQueryUIUri.toString())
+			.replace("{{tabulator_js}}", tabulatorUri.toString())
+			.replace("{{yatt_js}}", yatt_js.toString())
+			.replace("{{tabulator_css}}", tabulatorCssUri.toString())
+			.replace("{{progress_tracker_css}}", progressTrackerUri.toString());
 		return yattViewerHtml;
-	}		
+	}
 }
 
 function getNonce() {
